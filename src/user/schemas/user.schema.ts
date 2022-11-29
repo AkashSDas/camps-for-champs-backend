df6ebbe0f48fe674a14d5d3a9f50c023f529edc4
AkashSDas <aakashdas368@gmail.com>
2022-11-29 21:21:18 +0530

    add instance methods and hooks

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,6 +1,9 @@
 import { isEmail } from "class-validator";
+import crypto from "crypto";
 import { Document, SchemaTypes } from "mongoose";
 
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
 /**
@@ -9,6 +12,10 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
  */
 @Schema({ timestamps: true })
 export class User extends Document {
+  constructor(private jwt: JwtService, private config: ConfigService) {
+    super();
+  }
+
   @Prop({ type: SchemaTypes.String, max: 64, trim: true })
   firstName?: string;
 
@@ -25,7 +32,7 @@ export class User extends Document {
   passwordResetToken?: string;
 
   @Prop({ type: SchemaTypes.Date, select: false })
-  passwordResetTokenExpires?: Date;
+  passwordResetTokenExpiresAt?: Date;
 
   @Prop({ type: SchemaTypes.Boolean, default: false })
   active: boolean;
@@ -37,7 +44,40 @@ export class User extends Document {
   verificationToken?: string;
 
   @Prop({ type: SchemaTypes.Date, select: false })
-  verificationTokenExpires?: Date;
+  verificationTokenExpiresAt?: Date;
+
+  // ============================
+  // INSTANCE METHODS
+  // ============================
+
+  /**
+   * Generate a random token, hash it and set it as verification token
+   * along with its expiry date (10min from now)
+   *
+   * @returns the generated token
+   */
+  generateVerificationToken(): string {
+    var token = crypto.randomBytes(32).toString("hex");
+
+    this.verificationToken = crypto
+      .createHash("sha256")
+      .update(token)
+      .digest("hex");
+
+    // 10 minutes
+    this.verificationTokenExpiresAt = new Date(Date.now() + 10 * 60 * 1000);
+
+    return token;
+  }
+
+  /** Genereate access token for JWT authentication. Short duration */
+  accessToken(): string {
+    var payload = { _id: this._id, email: this.email };
+    return this.jwt.sign(payload, {
+      expiresIn: this.config.get("JWT_SECRET_EXPIRES_IN"),
+      secret: this.config.get("JWT_SECRET"),
+    });
+  }
 }
 
 export var UserSchema = SchemaFactory.createForClass(User);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,55 @@
+import * as argon from "argon2";
+
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
 import { User, UserSchema } from "./schemas";
+import { UserRepository } from "./user.repository";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: function (repository: UserRepository) {
+          var schema = UserSchema;
+
+          // PRE HOOK
+          schema.pre("save", async function preMongooseSave(next) {
+            // If password is modified then hash it
+            if (this.isModified("password")) {
+              this.password = await argon.hash(this.password);
+            }
+
+            // Validate email uniqueness (could add other field which needs to be unique)
+            if (this.isModified("email")) {
+              let query = [];
+              if (this.isModified("email")) query.push({ email: this.email });
+              let exists = await repository.exists({ $or: query });
+              if (exists) return next(new Error("Duplicate"));
+            }
+
+            return next();
+          });
+
+          // POST HOOK
+          schema.post("save", function handleDuplicateError(err, user, next) {
+            // Handle error due to violation of unique fields
+            if (err instanceof Error && err.message == "Duplicate") {
+              return next(new Error("Email already exists"));
+            }
+
+            if (err.name === "MongoError" && err.code === 11000) {
+              return next(new Error("Duplicate fields"));
+            }
+
+            return next();
+          });
+
+          return schema;
+        },
+      },
+    ]),
   ],
 })
 export class UserModule {}
