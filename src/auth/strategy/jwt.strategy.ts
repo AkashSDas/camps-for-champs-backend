d0cd15f1c4a9832aee58526daab4a0d1df4fdb80
AkashSDas <aakashdas368@gmail.com>
2022-11-30 12:38:14 +0530

    add pkgs, update app and fix jwt strategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,18 +1,19 @@
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { UserRepository } from "src/user/user.repository";
 
-import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 
+// Using `process.env` instead of `ConfigService` because ConfigModule's env variables
+// are only available at run time but not on the nestjs initial state.
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
-  constructor(config: ConfigService, private repository: UserRepository) {
+  constructor(private repository: UserRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         ExtractJwt.fromAuthHeaderAsBearerToken(),
-        ExtractJwt.fromUrlQueryParameter("access_token"),
+        ExtractJwt.fromUrlQueryParameter("accessToken"),
       ]),
       ignoreException: false,
-      secretOrKey: config.get("JWT_SECRET"),
+      secretOrKey: process.env.JWT_SECRET,
       passReqToCallback: true,
     });
   }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as cookieParser from "cookie-parser";
+
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
@@ -6,10 +8,9 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   var app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("/api");
-
-  // Enable validation and strip out any properties that are not in the DTO
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // Enable validation and strip out any properties that are not in the DTO
+  app.use(cookieParser());
+  app.enableCors({ credentials: true, origin: process.env.FRONTEND_URL });
   await app.listen(8000);
 }
 bootstrap();
