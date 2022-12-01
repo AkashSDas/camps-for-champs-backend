ddf1f55a07ca49587969730dc20e5af8c84895cc
AkashSDas <aakashdas368@gmail.com>
2022-12-01 12:52:01 +0530

    fix filename and implement models

--- src/camp/schemas/camp-booking.schema.ts
+++ src/camp/schemas/camp-booking.schema.ts
@@ -1,11 +1,57 @@
+import { SchemaTypes } from "mongoose";
+import { User } from "src/user/schemas";
+
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+import { Camp } from "./";
+
+export enum CampBookingStatus {
+  FULFILLED = "fulfilled",
+  CANCELLED = "cancelled",
+  PENDING = "pending",
+}
+
+@Schema({ _id: false })
+class CampGuest {
+  @Prop({ type: Number, min: 0, required: true })
+  adult: number;
+
+  @Prop({ type: Number, min: 0, required: true })
+  child: number;
+
+  @Prop({ type: Number, min: 0, required: true })
+  pet: number;
+}
+
+var campGuestSchema = SchemaFactory.createForClass(CampGuest);
+
 export class CampBooking extends Document {
+  @Prop({ type: SchemaTypes.ObjectId, ref: Camp.name, required: true })
   camp: string;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: User.name, required: true })
   user: string;
+
+  @Prop({ type: Date, required: true })
   checkInDate: Date;
+
+  @Prop({ type: Date, required: true })
   checkOutDate: Date;
-  guests: { adult: number; child: number; pet: number };
+
+  @Prop({ type: [campGuestSchema], required: true, default: [] })
+  guests: CampGuest[];
+
+  @Prop({ type: Number, default: 0, min: 0, required: true })
   campUnits: number;
+
+  @Prop({ type: Number, default: 0, min: 0, required: true })
   amount: number;
-  status: "fulfilled" | "cancelled" | "pending";
-  orderValue: "high" | "low" | "medium"; // depends on the type of camp and etc
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: CampBookingStatus,
+    default: CampBookingStatus.PENDING,
+  })
+  status: CampBookingStatus;
 }
--- src/camp/schemas/camp-wishlist.schema.ts
+++ src/camp/schemas/camp-wishlist.schema.ts
@@ -1,4 +1,17 @@
-export class CampWhislist extends Document {
+import { SchemaTypes } from "mongoose";
+import { User } from "src/user/schemas";
+
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+import { Camp } from "./";
+
+@Schema()
+export class CampWishlist extends Document {
+  @Prop({ type: SchemaTypes.ObjectId, ref: User.name, required: true })
   user: string;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: Camp.name, required: true })
   camp: string;
 }
+
+export var campWishlist = SchemaFactory.createForClass(CampWishlist);
