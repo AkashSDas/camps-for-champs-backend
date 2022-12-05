d27ac9d7f9605a37b6a1610766e881364b2c420f
AkashSDas <aakashdas368@gmail.com>
2022-12-05 07:17:31 +0530

    rm sample module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,15 @@ import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 
 import { AuthModule } from "./auth/auth.module";
+import { CampBookingModule } from "./camp-booking/camp-booking.module";
 import { CampWishlistModule } from "./camp-wishlist/camp-wishlist.module";
 import { CampModule } from "./camp/camp.module";
-import { SampleModule } from "./sample/sample.module";
 import { UserModule } from "./user/user.module";
-import { CampBookingModule } from './camp-booking/camp-booking.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_CONNECT_URL),
-    SampleModule,
     UserModule,
     AuthModule,
     CampModule,
