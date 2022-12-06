b2a2b00719feecd903027258f8ccfabe034dfa74
AkashSDas <aakashdas368@gmail.com>
2022-12-06 21:06:48 +0530

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,30 @@ import * as morgan from "morgan";
 
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   var app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix("/api");
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // Enable validation and strip out any properties that are not in the DTO
-  app.use(cookieParser());
-  app.use(morgan("tiny"));
-  app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
   app.enableCors({ credentials: true, origin: process.env.FRONTEND_URL });
+
+  app.use(cookieParser());
+  app.use(morgan("dev"));
+  app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
+
+  var config = new DocumentBuilder()
+    .setTitle("Camps for Champs")
+    .setDescription("RESTful back-end for main operations of Camps for Champs")
+    .setVersion("1.0")
+    .addTag("camps")
+    .build();
+  var document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(5002);
 }
 bootstrap();
