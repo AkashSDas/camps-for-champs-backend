cbfb0b93c2d0752593ef42347cc2a2c2bc20d733
AkashSDas <aakashdas368@gmail.com>
2023-01-07 20:38:25 +0530

    v2 - initial setup

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  var app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 8000);
+}
+
+bootstrap();
