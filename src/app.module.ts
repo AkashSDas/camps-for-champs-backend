6627f61880f363fb438b94ef6237529dfee464f1
AkashSDas <aakashdas368@gmail.com>
2023-01-07 20:45:50 +0530

    setup mongoose, config, class validators and morgan

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECT_URL),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import * as morgan from "morgan";
+
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   var app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix("api");
+  app.enableCors({ credentials: true, origin: process.env.FRONTEND_URL });
+
+  // Enable validation and strip out any properties that are not in the DTO
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(morgan("dev"));
+
   await app.listen(process.env.PORT || 8000);
 }
 
