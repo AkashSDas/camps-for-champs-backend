51d7dd76bda5b8ec7c8fbc8dbf67680562a88bd4
AkashSDas <aakashdas368@gmail.com>
2023-01-09 12:48:38 +0530

    add password strength check

--- src/auth/dto/email-and-password.dto.ts
+++ src/auth/dto/email-and-password.dto.ts
@@ -1,4 +1,5 @@
-import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+// eslint-disable-next-line prettier/prettier
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from "class-validator";
 
 export class EmailAndPasswordSignupDto {
   @IsEmail()
@@ -6,6 +7,10 @@ export class EmailAndPasswordSignupDto {
   @IsNotEmpty()
   email: string;
 
+  @Matches(
+    /^(?=(.*[a-z]){3,})(?=(.*[A-Z]){2,})(?=(.*[0-9]){2,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$/,
+    { message: "Password is weak" },
+  )
   @Length(8, 20)
   @IsString()
   @IsNotEmpty()
