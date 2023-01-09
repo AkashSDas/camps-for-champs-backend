4c9b41bc110d8070bfb09b12e04ea7447353b615
AkashSDas <aakashdas368@gmail.com>
2023-01-09 11:06:51 +0530

    setup auth app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { MongooseModule } from "@nestjs/mongoose";
 
 import { TestingModule } from "./testing/testing.module";
 import { UserModule } from "./user/user.module";
+import { ServiceController } from './auth/service/service.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,8 +17,9 @@ import { UserModule } from "./user/user.module";
     // Add your modules here
     TestingModule,
     UserModule,
+    AuthModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [ServiceController, AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}
