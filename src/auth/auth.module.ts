4f59a5d9f815211babb9eaff6ef5bbd6308582f2
AkashSDas <aakashdas368@gmail.com>
2023-01-09 11:20:12 +0530

    add import and providers in user module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,16 @@
+import { UserModule } from "src/user/user.module";
+
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { RefreshTokenGuard } from "./guard";
+import { AccessTokenStrategy } from "./strategy";
 
 @Module({
+  imports: [UserModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AccessTokenStrategy, RefreshTokenGuard],
 })
 export class AuthModule {}
