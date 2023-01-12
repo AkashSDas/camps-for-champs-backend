fbe420203cdf73fbbf0652230230d4a5b84cb243
AkashSDas <aakashdas368@gmail.com>
2023-01-12 08:35:56 +0530

    fix auth providers list

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,8 @@ import { JwtModule } from "@nestjs/jwt";
 import { UserModule } from "../user/user.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { RefreshTokenGuard } from "./guard";
 // eslint-disable-next-line prettier/prettier
-import { AccessTokenStrategy, GoogleLoginStrategy, GoogleSignupStrategy } from "./strategy";
+import { AccessTokenStrategy, GoogleLoginStrategy, GoogleSignupStrategy, RefreshTokenStrategy } from "./strategy";
 
 @Module({
   imports: [UserModule, JwtModule.register({})],
@@ -14,7 +13,7 @@ import { AccessTokenStrategy, GoogleLoginStrategy, GoogleSignupStrategy } from "
   providers: [
     AuthService,
     AccessTokenStrategy,
-    RefreshTokenGuard,
+    RefreshTokenStrategy,
     GoogleSignupStrategy,
     GoogleLoginStrategy,
   ],
