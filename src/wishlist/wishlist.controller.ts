1031c5aa3172e526eef0dced1c441eddfb3a9428
AkashSDas <aakashdas368@gmail.com>
2023-01-17 23:39:11 +0530

    add feature to get and create wishlist

--- src/wishlist/wishlist.controller.ts
+++ src/wishlist/wishlist.controller.ts
@@ -1,8 +1,37 @@
-import { Controller } from "@nestjs/common";
+import { Request } from "express";
+import { AccessTokenGuard } from "src/auth/guard";
+import { User } from "src/user/schema";
+
+// eslint-disable-next-line prettier/prettier
+import { BadRequestException, Controller, Get, HttpCode, HttpStatus, InternalServerErrorException, Post, Req, UseGuards } from "@nestjs/common";
 
 import { WishlistService } from "./wishlist.service";
 
 @Controller("/v2/wishlist")
 export class WishlistController {
   constructor(private service: WishlistService) {}
+
+  @Get("user/:userId")
+  @UseGuards(AccessTokenGuard)
+  async getWishlistForUser(@Req() req: Request) {
+    var result = await this.service.getWishlistForUser((req.user as User)._id);
+    if (!result) {
+      throw new InternalServerErrorException("Failed to get wishlist for user");
+    }
+
+    return { wishlist: result };
+  }
+
+  @Post("camp/:campId")
+  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(AccessTokenGuard)
+  async createWishlistForUser(@Req() req: Request) {
+    var result = await this.service.createWishlistForUser(
+      (req.user as User)._id,
+      req.params.campId,
+    );
+
+    if (result instanceof Error) throw new BadRequestException(result.message);
+    return { wishlist: result };
+  }
 }
--- src/wishlist/wishlist.service.ts
+++ src/wishlist/wishlist.service.ts
@@ -1,8 +1,37 @@
+import { Types } from "mongoose";
+import { CampRepository } from "src/camp/camp.repository";
+
 import { Injectable } from "@nestjs/common";
 
 import { WishlistRepository } from "./wishlist.repository";
 
 @Injectable()
 export class WishlistService {
-  constructor(private repository: WishlistRepository) {}
+  constructor(
+    private repository: WishlistRepository,
+    private campRepository: CampRepository,
+  ) {}
+
+  async getWishlistForUser(id: Types.ObjectId) {
+    return await this.repository.find({ user: id });
+  }
+
+  async createWishlistForUser(userId: Types.ObjectId, campId: string) {
+    // Check if the camp exists
+    var campExists = await this.campRepository.exists({ campId });
+    if (!campExists) return new Error("Camp not found");
+
+    // Check if the wishlist already exists
+    var exists = await this.repository.exists({
+      user: userId,
+      camp: campExists._id,
+    });
+    if (exists) return new Error("Wishlist already exists");
+
+    // Create the wishlist
+    return await this.repository.create({
+      user: userId,
+      camp: campExists._id,
+    });
+  }
 }
